2f6dde4574b7c0145752d03dfcab63c45b51e560
saikou <svikoubah2k03@gmail.com>
2024-08-05 11:30:13 +0000

    becoming that goat in js

---
+++ Generateur/script.js
@@ -0,0 +1,19 @@
+const quotes = [
+  'HTML est une perte de temps',
+  'CSS encore plus',
+  'React fait des miracles',
+  'Laravel écrase php'
+];
+
+
+function renderquotes() {
+
+  for (let i = 0; i < quotess.length; i++) { 
+
+  } 
+
+  document.querySelector('quoteBtn') 
+    .innerHTML =  quotes.values;
+  }
+
+  console.log(quotes);
