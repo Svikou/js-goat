ac1ff6c4b854a65c0b18395888c94a00bec54f8a
saikou <svikoubah2k03@gmail.com>
2024-08-12 12:27:32 +0000

    updates

--- Carte à gratter/index.js
+++ Carte à gratter/index.js
@@ -3,8 +3,8 @@ let context = canvas.getContext("2d");
 
 const init = () => {
   let gradientColor = context.createLinearGradient(0, 0, 135, 135);
-  gradientColor.addColorStop(0, "#c3a3f1");
-  gradientColor.addColorStop(1, "#6414e9");
+  gradientColor.addColorStop(0, "#56f1ec");
+  gradientColor.addColorStop(1, "#b3e2db");
   context.fillStyle = gradientColor;
   context.fillRect(0, 0, 200, 200);
 };
